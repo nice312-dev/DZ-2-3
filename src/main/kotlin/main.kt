02eb778f412a5152671ea9bfd843bce8ed9cad9a
nice312-dev <74597002+nice312-dev@users.noreply.github.com>
2020-12-15 12:50:51 +0500

    Update main.kt

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,21 +1,21 @@
 fun main() {
     val itemPrice = 100
-    val itemCount = 9
-    val discount = 100
-    val discountStart = 1_000
-    val itemCount1 = 15
+//    val itemCount = 9
+//    val discount = 100
+//    val discountStart = 1_000
+//    val itemCount1 = 15
     val itemCount2 = 130
     val discount1 = 0.05
     val discount2 = 0.01
     val discountStart1 = 10_000
 
-    val totalPrice = itemPrice * itemCount
-    val result = if (totalPrice > discountStart) totalPrice - discount else totalPrice
-    println("Total Price: $result")
+//    val totalPrice = itemPrice * itemCount
+//    val result = if (totalPrice > discountStart) totalPrice - discount else totalPrice
+//    println("Total Price: $result")
 
-    val totalPrice1 = itemPrice * itemCount1
-    val result1 = if (totalPrice1 > discountStart) totalPrice1 - discount else totalPrice
-    println("Total Price: $result1")
+//    val totalPrice1 = itemPrice * itemCount1
+//    val result1 = if (totalPrice1 > discountStart) totalPrice1 - discount else totalPrice
+//    println("Total Price: $result1")
 
     val totalPrice2 = itemPrice * itemCount2
     val sale = if (totalPrice2 > discountStart1) totalPrice2 * discount1 else totalPrice2.toDouble()
@@ -24,7 +24,8 @@ fun main() {
     val totalSale1 = sale + sale1
     val result2 = totalPrice2 - totalSale1
 
-    println("Sale 5%: $sale")
+//    println("Sale 5%: $sale")
+    println("Total Price: $totalPrice2")
     println("Sale 5% + 1%: $totalSale1")
     println("Total Price: $result2")
 }
