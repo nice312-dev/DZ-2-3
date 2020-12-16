ad53ef8cd551e3203be7f085ef8348651590e9c0
nice312-dev <74597002+nice312-dev@users.noreply.github.com>
2020-12-16 16:53:07 +0500

    Update main.kt

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,31 +1,23 @@
 fun main() {
     val itemPrice = 100
-//    val itemCount = 9
-//    val discount = 100
-//    val discountStart = 1_000
-//    val itemCount1 = 15
-    val itemCount2 = 130
-    val discount1 = 0.05
-    val discount2 = 0.01
-    val discountStart1 = 10_000
-
-//    val totalPrice = itemPrice * itemCount
-//    val result = if (totalPrice > discountStart) totalPrice - discount else totalPrice
-//    println("Total Price: $result")
-
-//    val totalPrice1 = itemPrice * itemCount1
-//    val result1 = if (totalPrice1 > discountStart) totalPrice1 - discount else totalPrice
-//    println("Total Price: $result1")
-
-    val totalPrice2 = itemPrice * itemCount2
-    val sale = if (totalPrice2 > discountStart1) totalPrice2 * discount1 else totalPrice2.toDouble()
-    val totalSale = totalPrice2 - sale
-    val sale1 = totalSale * discount2
-    val totalSale1 = sale + sale1
-    val result2 = totalPrice2 - totalSale1
-
-//    println("Sale 5%: $sale")
-    println("Total Price: $totalPrice2")
-    println("Sale 5% + 1%: $totalSale1")
-    println("Total Price: $result2")
+    val itemCount = 130
+    val discountMeloman = calculateDiscountMeloman(itemPrice, itemCount, "meloman")
+    println("Pay: $discountMeloman")
 }
+fun calculateDiscountMeloman(itemPrice: Int, itemCount: Int, isMeloman: String): Double {
+    val shoppingSum = itemPrice * itemCount
+    val discount = calculateDiscount(shoppingSum)
+    val levelDiscount = calculateLevelDiscount(isMeloman)
+    return discount - (discount * levelDiscount)
+}
+fun calculateDiscount(shoppingSum: Int): Double =
+    when {
+        shoppingSum <= 1000 -> shoppingSum.toDouble()
+        shoppingSum in 1001..9999 -> shoppingSum - 100.0
+        else -> shoppingSum * 0.95
+    }
+fun calculateLevelDiscount(isMeloman: String): Double =
+    when (isMeloman) {
+        "meloman" -> 0.01
+        else -> 0.00
+    }
